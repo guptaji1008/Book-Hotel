76cd9d19c2131ea4955bfcfc553f5084675ebaa8
guptaji1008 <132588314+guptaji1008@users.noreply.github.com>
2024-02-19 14:38:21 +0530

    updated urls

--- next.config.js
+++ next.config.js
@@ -3,9 +3,9 @@ const nextConfig = {
     env: {
         DB_LOCAL_URI: "mongodb+srv://hotelbooking:hotelbooking123@cluster0.gqdumua.mongodb.net/hotel-booking?retryWrites=true&w=majority",
         DB_URI: "mongodb+srv://hotelbooking:hotelbooking123@cluster0.gqdumua.mongodb.net/hotel-booking?retryWrites=true&w=majority",
-        API_URL: "https://hotelbooker.netlify.app",
+        API_URL: "https://bookhotels.vercel.app",
         NEXTAUTH_SECRET: "AMANKUMARGUPTAISAMECHANICALENGINEERFROMIMSENGINEERINGCOLLEGE",
-        NEXTAUTH_URL: "https://hotelbooker.netlify.app",
+        NEXTAUTH_URL: "https://bookhotels.vercel.app",
         CLOUDINARY_CLOUD_NAME: "dbrd2j2lo",
         CLOUDINARY_API_KEY: "544732963741955",
         CLOUDINARY_API_SECRET: "LlkrKhmuE2klXfqTwmDXDblZvWo",
@@ -16,7 +16,7 @@ const nextConfig = {
         EMAIL_FROM_EMAIL: "noreply@bookhotels.com",
         EMAIL_FROM_NAME: "BOOK HOTELS",
         STRIPE_SECRET_KEY: "sk_test_51OgmFRSD4Cc8c1pOsFfMzC7gNNCPd3JQfWffb7tBQFGbni6Yh3vzfIqeBaDYGXOdaNgzHeKykc1ctZ6nNvhqiCzn00ejXvU6Vj",
-        STRIPE_WEBHOOKS_SECRET: "whsec_c963ca81a633be477cc1f663214c37c043cd7e5133a31a7755f54677c1c2bf97",
+        STRIPE_WEBHOOKS_SECRET: "whsec_yhkCK4Qm41COABcVH0gJeW1xRHJ2JV7S",
         REVALIDATE_TOKEN: "AMANKUMARGUPTAMECHANICALENGINEER27121998"
     },
     images: {
